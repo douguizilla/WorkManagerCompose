311696a518c29f7203ffecf5fcd1ad2a435c5716
Douglas Gomes <douglasgommez@gmail.com>
2022-03-16 19:57:01 -0300

    create downloadrequest of OneTimeWorkRequest setting to do when have network connection

--- app/src/main/java/com/example/workmanagercompose/MainActivity.kt
+++ app/src/main/java/com/example/workmanagercompose/MainActivity.kt
@@ -10,11 +10,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import com.example.workmanagercompose.ui.theme.WorkManagerComposeTheme
+import java.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val downloadRequest = OneTimeWorkRequestBuilder<DownloadWorker>()
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(
+                        NetworkType.CONNECTED
+                    )
+                    .build()
+            )
         setContent {
             WorkManagerComposeTheme {
 
