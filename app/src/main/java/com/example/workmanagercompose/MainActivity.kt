c38dfa77c8715848435007f96a8809ad1c70b8e0
Douglas Gomes <douglasgommez@gmail.com>
2022-03-21 20:26:03 -0300

    Observe value of work as a state, implementing download and filters infos to build a imageUri

--- app/src/main/java/com/example/workmanagercompose/MainActivity.kt
+++ app/src/main/java/com/example/workmanagercompose/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.workmanagercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.work.*
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.core.net.toUri
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.workmanagercompose.ui.theme.WorkManagerComposeTheme
-import java.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +27,40 @@ class MainActivity : ComponentActivity() {
             )
             .build()
 
-        val colorFilterWorker = OneTimeWorkRequestBuilder<ColorFilterWorker>()
+        val colorFilterRequest = OneTimeWorkRequestBuilder<ColorFilterWorker>()
             .build()
 
         val workManager = WorkManager.getInstance(applicationContext)
 
         setContent {
             WorkManagerComposeTheme {
+                val workInfos = workManager
+                    .getWorkInfosForUniqueWorkLiveData("download")
+                    .observeAsState()
+                    .value
+
+                val downloadInfo = remember(key1 = workInfos){
+                    workInfos?.find { it.id == downloadRequest.id}
+                }
+
+                val filterInfo = remember(key1 = workInfos){
+                    workInfos?.find { it.id == colorFilterRequest.id}
+                }
+
+                val imageUri by derivedStateOf {
+                    val downloadUri = downloadInfo
+                        ?.outputData
+                        ?.getString(WorkerKeys.IMAGE_URI)
+                        ?.toUri()
+
+                    val filterUri = filterInfo
+                        ?.outputData
+                        ?.getString(WorkerKeys.FILTER_URI)
+                        ?.toUri()
+
+                    filterUri ?: downloadUri
+                }
+
 
             }
         }
